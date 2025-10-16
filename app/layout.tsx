9104bbcc0a4e6bf978b74698acb75946db0249bb
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-16 21:38:53 +0500

    Minor Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import {Inter } from "next/font/google";
 import '@/assets/styles/globals.css';
-
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 const inter = Inter({ subsets : ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modren e-commerce website biult with Next.js",
+  title: 
+  {template: `%S | Prostore`,
+    default: APP_NAME,
+  },
+
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+export const APP_DESCRIPTION = process.env.
+NEXT_PUBLIC_APP_DESCRIPTION || 'A modern e-commerce website built with Next.js';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
