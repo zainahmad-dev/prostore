b8da7a709ac4afe727df3a37bfd7c4fb6ac47117
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-17 17:49:43 +0500

    minor changes

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,45 +1,29 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-
-import ModeToggle from "./mode-toggle";
-
-
-
+import Menu from "./menu";
 const Header = () => {
-    return <header className="w-full border-b">
-    <div className="wrapper flex-between">
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
         <div className="flex-start">
-            <Link href='/' className='flex-start'>
-             <Image 
-             src='/images/logo.svg'
-             alt={`${APP_NAME} logo`}
-             height={48}
-             width={48}
-             priority={true}
-             />
-              <span className="hidden lg:block font-bold text-2xl ml-3">
-                {APP_NAME}
-              </span>
-            </Link>
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
         </div>
-        <div className="space-x-2">
-            <ModeToggle />
-            <Button asChild variant='ghost'>
-                <Link href='/cart'>
-                <ShoppingCart /> Cart
-                </Link>
-             </Button>
-             <Button asChild >
-                <Link href='/sign-in'>
-                <UserIcon/> Sign In
-                </Link>
-             </Button>
-        </div>
-    </div>
-    </header>;
-}
- 
+        <Menu />
+      </div>
+    </header>
+  );
+};
+
 export default Header;
