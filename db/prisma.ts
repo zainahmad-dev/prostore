40808b053213c6badacba32c514fd69aa85be931
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-18 23:38:39 +0500

    First Commit

---
+++ db/prisma.ts
@@ -0,0 +1,32 @@
+import { neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+
+// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+console.log('connectionString: ', connectionString);
+
+// Instantiates the Prisma adapter using the Neon connection string.
+const adapter = new PrismaNeon({ connectionString });
+
+// Extends the PrismaClient with a custom result transformer to convert
+// the price and rating fields to strings.
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
