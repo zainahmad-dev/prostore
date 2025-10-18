2e680eaaa03c6f85c29ee7358bedc4167f871809
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-18 17:12:04 +0500

    First Commit

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,18 @@
+'use server';
+import { PrismaClient } from "@prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+//Get Latest Products
+
+export async function getLatestProducts() {
+    const prisma = new PrismaClient();
+
+    const data = await prisma.product.findMany({
+        take: LATEST_PRODUCTS_LIMIT,
+        orderBy: {
+            createdAt: 'desc',
+        },
+        
+    });
+    return convertToPlainObject(data);
+}
