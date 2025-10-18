333d59a6211b5a7fbd1d9cf781d9bf3d2d946eb7
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-18 11:41:47 +0500

    minor change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const Homepage = () => {
- 
+ console.log(sampleData);
   return <>
-      prostore
+<ProductList data={sampleData.products} title="Newest Arrivals"/>
   </>;
 }
  
