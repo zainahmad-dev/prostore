d0b3d7335517e323c2ed4e2f84711379a32bddd6
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-18 17:12:49 +0500

    Minor Changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
 
-const Homepage = () => {
- console.log(sampleData);
+import { getLatestProducts } from "@/lib/actions/product.actions";
+const Homepage = async () => {
+ const latestProducts = await getLatestProducts();
   return <>
-<ProductList data={sampleData.products} title="Newest Arrivals"/>
+<ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
   </>;
 }
  
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Convert prisma object to regular js object
+
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value)) ;
+}
