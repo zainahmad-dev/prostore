ba06d65ec793283013df1a937e45c032f3f42e71
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-20 12:21:35 +0500

    Minor Changes

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,23 @@
+
+import { hashSync} from 'bcrypt-ts-edge'
+
+
 const sampleData = {
+users : [
+  {
+    name  : 'John',
+   email : 'admin@example.com',
+    password : hashSync('123456' , 10),
+    role: 'admin',
+  },
+    {
+    name  : 'Jane',
+   email : 'user@example.com',
+    password : hashSync('123456' , 10),
+    role: 'user',
+  }
+],
+
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
--- db/seed.ts
+++ db/seed.ts
@@ -4,9 +4,16 @@ import sampleData from './sample-data';
 
 async function main() {
 const prisma = new PrismaClient();
-await prisma.product.deleteMany({});
+await prisma.product.deleteMany();
+await prisma.account.deleteMany();
+await prisma.session.deleteMany();
+await prisma.verificationToken.deleteMany();
+await prisma.user.deleteMany();
     await prisma.product.createMany({ 
         data: sampleData.products,
+    });
+ await prisma.user.createMany({ 
+        data: sampleData.users,
     });
 
     console.log('Database has been seeded successfully.');}
