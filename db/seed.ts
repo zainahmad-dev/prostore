3fdb55a7528508591393fa2a9269c3da037d26bd
Zain Ahmad <zainahmad4708@gmail.com>
2025-10-21 09:40:26 +0500

    Minor changes

--- db/seed.ts
+++ db/seed.ts
@@ -4,11 +4,12 @@ import sampleData from './sample-data';
 
 async function main() {
 const prisma = new PrismaClient();
-await prisma.product.deleteMany();
-await prisma.account.deleteMany();
-await prisma.session.deleteMany();
-await prisma.verificationToken.deleteMany();
-await prisma.user.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
     await prisma.product.createMany({ 
         data: sampleData.products,
     });
