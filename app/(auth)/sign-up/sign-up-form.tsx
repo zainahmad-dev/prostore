67c1f94766ed9b28308497880df9ffb326f98ab3
Zain Ahmad <zainahmad4708@gmail.com>
2025-11-20 11:55:49 +0500

    First Commit

---
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signUpDefaultValues } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signUpUser} from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
+
+
+const SignUpForm = () => {
+
+    const [data,action] = useActionState(signUpUser,{
+       success: false,
+       message: ''
+    });
+
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+    const SignUpButton = () => {
+    const { pending } = useFormStatus();
+
+        return(
+            <Button disabled={pending} className="w-full" variant='default'>
+                 {pending ? 'Submitting...' : 'Sign Up'}
+            </Button>
+        )
+    }
+    return <>
+    <form action={action}>
+    <input type= "hidden" name="callbackUrl" value={callbackUrl} />
+    <div className="space-y-6">
+         <div className="space-y-1.5">
+        <Label htmlFor='name'>
+            Name
+            </Label>
+            <Input
+            id="name"
+            name="name"
+            type="text"
+            autoComplete="name"
+            defaultValue={signUpDefaultValues.name}
+            />
+    </div>
+    <div className="space-y-1.5">
+        <Label htmlFor='email'>
+            Email
+            </Label>
+            <Input
+            id="email"
+            name="email"
+            type="text"
+            autoComplete="email"
+            defaultValue={signUpDefaultValues.email}
+            />
+    </div>
+    <div className="space-y-1.5">
+        <Label htmlFor='password'>
+            Password
+            </Label>
+            <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signUpDefaultValues.password}
+            />
+    </div>
+    <div className="space-y-1.5">
+        <Label htmlFor='confirmPassword'>
+           Confirm Password
+            </Label>
+            <Input
+            id="ConfirmPassword"
+            name="ConfirmPassword"
+            type="password"
+            required
+            autoComplete="ConfirmPassword"
+            defaultValue={signUpDefaultValues.confirmPassword}
+            
+            />
+    </div>
+    <div>
+    <SignUpButton/>
+    </div>
+    {data && !data.success && (
+        <div className="text-center text-destructive">
+          {data.message}
+        </div>
+    )}
+    <div className="text-sm text-center text-muted-foreground">
+          Already have an account?{' '}
+        <Link href='/sign-in' target='_self' className="link">
+        Sign In
+        </Link>
+    </div>
+        </div>
+    </form>
+    </>;
+}
+ 
+export default SignUpForm;
