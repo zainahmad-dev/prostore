e68fe6b918f1a2c687017d64b0226d585a0b3fab
zainahmad-dev <zainahmad4708@gmail.com>
2026-01-08 16:41:21 +0500

    First Commit

---
+++ app/(root)/place-order/place-order-form.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Check, Loader } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useFormStatus } from 'react-dom';
+import { createOrder } from '@/lib/actions/order.action';
+
+const PlaceOrderForm = () => {
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const res = await createOrder();
+
+    if (res.redirectTo) {
+      router.push(res.redirectTo);
+    }
+  };
+
+  const PlaceOrderButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className='w-full'>
+        {pending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Check className='w-4 h-4' />
+        )}{' '}
+        Place Order
+      </Button>
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='w-full'>
+      <PlaceOrderButton />
+    </form>
+  );
+};
+
+export default PlaceOrderForm;
---
+++ lib/actions/order.action.ts
@@ -0,0 +1,96 @@
+"use server";
+
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { formatError } from "../utils";
+import { getMyCart } from "./cart.actions";
+import { auth } from "@/auth";
+import { getUserById } from "./user.actions";
+import { prisma } from "@/db/prisma";
+import { CartItem } from "@/types";
+import { insertOrderSchema } from "../validator";
+// Create Order and order items
+
+export async function createOrder() {
+  try {
+    const session = await auth();
+    if (!session) throw new Error("User is not authenticated");
+
+    const cart = await getMyCart();
+    const userId = session?.user?.id;
+    if (!userId) throw new Error("User not found");
+    const user = await getUserById(userId);
+
+    if (!cart || cart.items.length === 0) {
+      return {
+        success: false,
+        message: "Your cart is empty",
+        redirectTo: "/cart",
+      };
+    }
+    if (!user.address) {
+      return {
+        success: false,
+        message: "No shipping address",
+        redirectTo: "/shipping-address",
+      };
+    }
+
+    if (!user.paymentMethod) {
+      return {
+        success: false,
+        message: "No payment method",
+        redirectTo: "/payment-method",
+      };
+    }
+      // Create order object
+    const order = insertOrderSchema.parse({
+      userId: user.id,
+      shippingAddress: user.address,
+      paymentMethod: user.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+    });
+
+    // Create a transaction to create order and order items in database
+    const insertedOrderId = await prisma.$transaction(async (tx) => {
+      // Create order
+      const insertedOrder = await tx.order.create({ data: order });
+      // Create order items from the cart items
+      for (const item of cart.items as CartItem[]) {
+        await tx.orderItem.create({
+          data: {
+            ...item,
+            price: item.price,
+            orderId: insertedOrder.id,
+          },
+        });
+      }
+      // Clear cart
+      await tx.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: [],
+          totalPrice: 0,
+          taxPrice: 0,
+          shippingPrice: 0,
+          itemsPrice: 0,
+        },
+      });
+
+      return insertedOrder.id;
+    });
+
+    if (!insertedOrderId) throw new Error('Order not created');
+
+    return {
+      success: true,
+      message: 'Order created',
+      redirectTo: `/order/${insertedOrderId}`,
+    };
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    return { success: false, message: formatError(error) };
+  }
+}
