71204ccd970981d1cc1b995d878faa9cc9853fad
zainahmad-dev <zainahmad4708@gmail.com>
2026-02-20 09:16:36 +0500

    Minor Changes

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,8 @@
 'use server';
 import {prisma} from  '@/db/prisma';
 import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
+
 //Get Latest Products
 
   export async function getLatestProducts() {
@@ -22,4 +23,31 @@ where: {
 },
     }); 
 
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+  
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+  
+})  {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
