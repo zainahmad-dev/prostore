993d37eb706e03ea75f661d7e357fcac39baaf3c
zainahmad-dev <zainahmad4708@gmail.com>
2026-07-26 23:57:23 +0500

    Minor Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       >
           <ThemeProvider
           attribute="class"
-          defaultTheme="Light"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
